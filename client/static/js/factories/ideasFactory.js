74309730a7fba803d5a455c75a2b28119c276d16
Joe <txjoe75@gmail.com>
2016-08-23 16:41:21 -0500

    added Ideas Factory

---
+++ client/static/js/factories/ideasFactory.js
@@ -0,0 +1,29 @@
+console.log('Idea Factory');
+
+app.factory('ideaFactory', ['$http', function($http, $cookies){
+	function checkAndRun(callback){
+		return data => { if (typeof(callback) == 'function') callback(data.data);}
+	}
+	return {
+		index: (callback) => {
+			$http.get('/ideas')
+			.then(checkAndRun(callback))
+		},
+		create: (idea, callback) => {
+			$http.post('/ideas', idea)
+			.then(checkAndRun(callback))
+		},
+		update: (idea, callback) => {
+			$http.put('/ideas/'+idea._id, idea)
+			.then(checkAndRun(callback))
+		},
+		show: (ideaID, callback) => {
+			$http.get ('/ideas/'+ideaID)
+			.then (checkAndRun(callback))
+		},
+		delete: (ideaID, callback) => {
+			$http.delete ('/ideas/'+ideaID)
+			.then (checkAndRun(callback))
+		}
+ 	}
+}])
